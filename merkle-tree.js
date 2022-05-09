a3fa5e849c5c7299f0e23f63e7bfd8c13737d85c
proxyGrit <dev@proxygrit.com>
2022-05-08 22:44:17 -0700

    Add 'getProof' to verify node existing within merkle tree

--- merkle-tree.js
+++ merkle-tree.js
@@ -21,6 +21,44 @@ class MerkleTree {
     }
     return merkelize(this.leaves, this.concat);
   }
+  getProof(index) {
+    let merkleProof = [];
+    const calculateProof = (leaves, index) => {
+      if (leaves.length === 1) {
+        return merkleProof;
+      }
+
+      let pairing;
+      let left;
+      if (index % 2 === 0) {
+        pairing = leaves[index + 1];
+        left = false;
+      } else {
+        pairing = leaves[index - 1];
+        left = true;
+      }
+      if (pairing) {
+        merkleProof.push({ data: pairing, left });
+      }
+
+      let newLeaves = [];
+      let arr = [];
+      for (let i = 0; i < leaves.length; i++) {
+        if (arr.length < 2) {
+          arr.push(leaves[i]);
+          if (arr.length === 2) {
+            newLeaves.push(this.concat(arr[0], arr[1]));
+            arr = [];
+          }
+        }
+      }
+      if (arr.length) {
+        newLeaves.push(arr[0]);
+      }
+      return calculateProof(newLeaves, Math.floor(index / 2));
+    };
+    return calculateProof(this.leaves, index);
+  }
 }
 
 module.exports = MerkleTree;
